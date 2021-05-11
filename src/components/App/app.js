84dbb1901d02b5ea5bd12b70e206a76c01bad692
BastosGaming <bastien.leroy31@gmail.com>
2021-05-11 14:21:55 +0200

    initial commit

---
+++ src/components/App/app.js
@@ -0,0 +1,17 @@
+// == Import : npm
+
+// == Import : components
+
+// == Import : local
+import './style.scss';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            coucou
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+// == Import : npm
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// == Import : components
+import App from './components/App/app';
+
+// == Import : local
+import './styles/index.scss';
+import * as serviceWorker from './serviceWorker';
+
+const rootReactElement = (
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
+);
+const target = document.getElementById('root');
+
+render(rootReactElement, target);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
